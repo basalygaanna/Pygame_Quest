560255cb0fb9178198307ef076fe55a35a67ec39
anna-zot <118592865+anna-zot@users.noreply.github.com>
2022-12-24 23:53:14 +0300

    Add files via upload
    да

---
+++ 001.py
@@ -0,0 +1,300 @@
+import pygame, random
+
+class Board:
+    # создание поля
+    def __init__(self, width, height):
+
+        self.width = width
+
+        self.height = height
+
+        self.board = [[0] * width for _ in range(height)]
+
+        # значения по умолчанию
+
+        self.left = 0
+
+        self.top = 0
+
+        self.cell_size = 40
+
+    # настройка внешнего вида
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+
+        self.top = top
+
+        self.cell_size = cell_size
+
+
+    def render(self, screen):
+
+        # pygame.draw.circle(screen, (255, 255, 0), 70, 80)
+        for y in range(self.height):
+
+            for x in range(self.width):
+
+                coord = (self.left + x * self.cell_size, self.top + y * self.cell_size, self.cell_size, self.cell_size)
+
+                if self.board[y][x] == 1:
+                    pygame.draw.rect(screen, (0, 255, 0), coord)
+
+                elif self.board[y][x] == 2:
+                    pygame.draw.rect(screen, (255, 255, 0), coord)
+
+                else:
+                    pygame.draw.rect(screen, (255, 255, 255), coord, 1)
+    def get_cell(self, coord):
+        x, y = coord[0] - self.left, coord[1] - self.top
+
+        if self.width * self.cell_size > x >= 0 and self.height * self.cell_size > y >= 0:
+
+            x, y = x // self.cell_size, y // self.cell_size
+
+            self.board[y][x] = (self.board[y][x] + 1) % 3
+
+        else:
+            print(None)
+
+    def get_col(self, coord):
+        x, y = coord[0] - self.left, coord[1] - self.top
+
+        x, y = x // self.cell_size, y // self.cell_size
+        self.cake = 0
+
+        if player.get_go() == 'a':
+            x -= 1
+
+        elif player.get_go() == 's':
+            y += 1
+
+        elif player.get_go() == 'd':
+            x += 1
+
+        else:
+            y -= 1
+        print('get_col')
+
+        print('lllol')
+        if self.board[y][x]:
+
+            if self.board[y][x] == 2:
+                self.cake = 2
+
+            else:
+                self.cake = 1
+            print(True)
+
+            return True
+
+        else:
+            print(False)
+            return False
+
+
+def draw(screen):
+    screen.fill((0, 0, 0))
+
+
+class Arrow:
+    def __init__(self, pos):
+        self.cs = 20
+        coord = (pos[0], pos[1], self.cs * 2, self.cs)
+        pygame.draw.rect(screen, (255, 0, 0), coord)
+
+
+    def get_go(self):
+        if go[0]:
+            print('s')
+            return 's'
+
+        if go[1]:
+            print('w')
+            return 'w'
+
+        if go[2]:
+            print('a')
+            return 'a'
+
+        if go[3]:
+            print('d')
+            return 'd'
+
+
+    def render(self, pos):
+        coord = (pos[0], pos[1], self.cs * 2, self.cs)
+
+        if self.get_go() == 's':
+            pygame.draw.rect(screen, (255, 0, 0), coord)
+
+        elif self.get_go() == 'w':
+            pygame.draw.rect(screen, (255, 255, 0), coord)
+
+        elif self.get_go() == 'a':
+            pygame.draw.rect(screen, (255, 0, 255), coord)
+
+        elif self.get_go() == 'd':
+            pygame.draw.rect(screen, (0, 255, 255), coord)
+
+        else:
+            pygame.draw.rect(screen, (0, 0, 255), coord)
+
+    def do(self, coord):
+        print('do')
+
+        if board.get_col(coord) and board.cake == 2:
+            print('cool')
+            self.i_can_speak()
+
+        else:
+            print('notcool')
+
+    def i_can_speak(self):
+        font = pygame.font.Font(None, 100)
+        r = random.randint(1, 20)
+        print('pl')
+        print(r)
+
+        if r <= 5:
+            text = font.render("Это диалог", True, (100, 255, 100))
+
+        elif r <= 10:
+            text = font.render("Это диалг", True, (100, 255, 100))
+
+        elif r <= 13:
+            text = font.render("Этот диалог", True, (100, 255, 100))
+
+        elif r <= 16:
+            text = font.render("Это диалог...", True, (100, 255, 100))
+
+        elif r <= 19:
+            text = font.render("Это диалог :)", True, (100, 255, 100))
+
+        else:
+            text = font.render("Это конец. Выходи.", True, (100, 255, 100))
+            text_x = width // 2 - text.get_width() // 2
+            text_y = height // 2 - text.get_height() // 2
+            screen.blit(text, (text_x, text_y))
+            pygame.display.flip()
+
+            while pygame.event.wait().type != pygame.QUIT:
+                pass
+            pygame.quit()
+        text_x = width // 2 - text.get_width() // 2
+        text_y = height // 2 - text.get_height() // 2
+        screen.blit(text, (text_x, text_y))
+        pygame.display.flip()
+
+        while pygame.event.wait().type != pygame.KEYDOWN:
+            pass
+
+
+if __name__ == '__main__':
+    # инициализация Pygame:
+
+    pygame.init()
+    # размеры окна:
+    fl = [0, 0, 0, 0]
+    go = [0, 0, 0, 0]
+
+    clock = pygame.time.Clock()
+    flag = False
+
+    size = width, height = 1200, 1200
+    FPS = 10
+
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    runt = 40
+
+    board = Board(40, 40)
+
+    player, pos = Arrow((100, 100)), (100, 100)
+
+    while running:
+        print(fl)
+        print(go)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                flag = True
+                p_s = event.pos
+                board.get_cell(p_s)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_z or event.key == pygame.K_RETURN:
+                    player.do(pos)
+
+                if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+                    FPS = 20
+
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    fl[0] = 1
+                    go = [0, 0, 0, 0]
+                    go[0] = 1
+
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    fl[1] = 1
+                    go = [0, 0, 0, 0]
+                    go[1] = 1
+
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    fl[2] = 1
+                    go = [0, 0, 0, 0]
+                    go[2] = 1
+
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    fl[3] = 1
+                    go = [0, 0, 0, 0]
+                    go[3] = 1
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT:
+                    FPS = 10
+
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    fl[0] = 0
+
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    fl[1] = 0
+
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    fl[2] = 0
+
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    fl[3] = 0
+
+        if player.get_go() == 's' and fl[0]:
+            if not board.get_col(pos):
+                pos = pos[0], pos[1] + runt
+
+        if player.get_go() == 'w' and fl[1]:
+            if not board.get_col(pos):
+                pos = pos[0], pos[1] - runt
+
+        if player.get_go() == 'a' and fl[2]:
+            if not board.get_col(pos):
+                pos = pos[0] - runt, pos[1]
+
+        if player.get_go() == 'd' and fl[3]:
+            if not board.get_col(pos):
+                pos = pos[0] + runt, pos[1]
+
+
+
+        screen.fill((0, 0, 0))
+
+        board.render(screen)
+
+        player.render(pos)
+
+        clock.tick(FPS)
+        
+        pygame.display.flip()
+
+    # завершение работы:
+    pygame.quit()
